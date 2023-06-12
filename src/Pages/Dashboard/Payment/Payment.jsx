eee205f3d29e6c580c2a03d81d0a2ca928cfc6a0
almuraduzzaman <almuraduzzaman.eee@gmail.com>
2023-06-12 23:08:59 +0600

    payment page edited

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -21,7 +21,6 @@ const Payment = () => {
                 <title>Payment | SpeakUpSummers</title>
             </Helmet>
             <SectionTitle subHeading="please process" heading="Payment"></SectionTitle>
-            <h2 className="text-3xl"> Teka o teka tumi uira uira aso...</h2>
             <Elements stripe={stripePromise}>
                 <CheckoutForm specificClass={specificClass} price={price}></CheckoutForm>
             </Elements>
